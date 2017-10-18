14f7ee4db41d425a46378485402e2634a3478899
Matthew Ouellette <mattouellette120@gmail.com>
2017-10-17 20:18:14 -0400

    Begin conversion to scanner input, redo For loop next

--- TipTracker.go
+++ TipTracker.go
@@ -2,44 +2,35 @@ package main
 
 import (
 	"fmt"
-	//"reflect"
+	"bufio"
+	"os"
+	"strconv"
+	"io"
 )
 
 var mileage float64 = 0.00
 var totalTips float64 = 0.00
-var tip 
 
 func main () {
+	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("What is the mileage rate?")
 	fmt.Scan(&mileage)
 	fmt.Println(fmt.Sprintf("Mileage is $%.2f", mileage))
-	fmt.Println("Input \"exit\" to end shift")
+	fmt.Println("Enter an empty string to end shift")
 	for numOfDel := 1.0 ; ; numOfDel++ {
 		fmt.Println(fmt.Sprintf("Enter the tip for delivery #%.0f", numOfDel))
-		fmt.Scan(&tip)
-/*		var tipType string = reflect.TypeOf(tip).String()
-		if tipType == "string" {
-			if tip == "exit" {
-				shiftDone = true
-				fmt.Println("End of shift")
-				fmt.Println(fmt.Sprintf("Number of deliveries: %d, Total Tips: $%.2f, Total Earnings: $%.2f", numOfDel, totalTips, totalTips+(mileage*numOfDel)))
-			} else {
-				fmt.Println("Invalid string")
-				numOfDel--
-			}
-		} else if  tipType == "float64" {
-				totalTips += tip
-		} else if tipeType == "int" {
-			floatTip := float64(tip)
-			totalTips += floatTip
-		} else {
-			fmt.Println("Invalid Input")
-			numOfDel--
-		}*/
-		if tip == "exit" {
+		tipString := scanner.Text()
+		if len(tipString) == 0 {
 			break
 		}
-		totalTips += tip
+		if err := scanner.Err(); err != nil {
+			if err != io.EOF {
+				fmt.Fprintln(os.Stderr, err)
+			}
+		}
+		if tip, err := strconv.ParseFloat(tipString, 64); err == nil {
+			totalTips += tip
+		}
 		fmt.Println(fmt.Sprintf("Total Tips: $%.2f", totalTips))
 		if numOfDel == 10 {
 			fmt.Println("End of shift")
