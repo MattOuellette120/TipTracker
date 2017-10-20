652502f121335db470657c1de8bd60f1b397c5c5
Matthew Ouellette <mattouellette120@gmail.com>
2017-10-20 18:47:39 -0400

    Clean up input acceptance.

--- TipTracker.go
+++ TipTracker.go
@@ -3,25 +3,42 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
-var mileage float64 = 0.00
+var mileageString string
 var totalTips float64 = 0.00
 var tipString string
+
 func main () {
-	fmt.Println("What is the mileage rate?")
-	fmt.Scan(&mileage)
+	fmt.Println("\"Mileage\" refers to the amount of money, per delivery, a driver is given for gas compensation.")
+	var mileage float64
+	for {
+		fmt.Println("How much is the current mileage?")
+		fmt.Scan(&mileageString)
+		cleanMileageString := strings.TrimPrefix(mileageString, "$")
+		m, err := strconv.ParseFloat(cleanMileageString, 64)
+		mileage = m
+		if err == nil {
+			break
+		} else {
+			fmt.Println("Mileage must be in the form of a float. Mileage is usually between $1.20 and $1.30")
+		}
+	}
 	fmt.Println(fmt.Sprintf("Mileage is $%.2f", mileage))
 	fmt.Println("Enter \"exit\" to end shift")
+	
 	for numOfDel := 1.0 ; ; numOfDel++ {
 		fmt.Println(fmt.Sprintf("Enter the tip for delivery #%.0f", numOfDel))
 		fmt.Scan(&tipString)
+		cleanTipString := strings.TrimPrefix(tipString, "$")
 		if tipString == "exit" {
+			numOfDel--
 			fmt.Println("End of shift")
-			fmt.Println(fmt.Sprintf("Number of deliveries: %.0f, Total Tips: $%.2f, Total Earnings: $%.2f", numOfDel, totalTips, totalTips+(mileage*numOfDel)))
+			fmt.Println(fmt.Sprintf("Number of deliveries: %.0f, Total Tips: $%.2f, Total Mileage Earned: $%.2f, Total Earnings: $%.2f", numOfDel, totalTips, numOfDel*mileage, totalTips+(mileage*numOfDel)))
 			break
 		}
-		tip, err := strconv.ParseFloat(tipString, 64)
+		tip, err := strconv.ParseFloat(cleanTipString, 64)
 		if err == nil {
 			totalTips += tip
 			fmt.Println(fmt.Sprintf("Total Tips: $%.2f", totalTips))
