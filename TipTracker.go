3c666e7fd69aeb7ab5b30837f927a18ebc97a5ba
Matthew Ouellette <mattouellette120@gmail.com>
2017-10-22 04:12:28 -0400

    Implement list and cancel commands. User can now view all orders and select one to cancel

--- TipTracker.go
+++ TipTracker.go
@@ -7,21 +7,20 @@ import (
 )
 
 //Variable Declaration
-var mileageString string
-var tipString string
-var helpString = "Command list:\nhelp		Displays this help menu\nexit		Ends the shift and displays the final totals"
+var inputString string
+var helpString = "Command list:\nhelp		Displays this help menu\nexit		Ends the shift and displays the final totals\nlist		Lists all current orders with ID and tip amount\ncancel		Allows user to cancel an order with its order ID. Cancelling an order sets the tip amount to $0"		//Help Menu
 
 
 func main () {
-	orders := make([]Order, 0, 1)
+	orders := make([]Order, 0, 1)		//Slice of all orders for the shift
 	
 	fmt.Println("\"Mileage\" refers to the amount of money, per delivery, a driver is given for gas compensation.") 
 	var mileage float64	//Declared mileage outside the loop so it can be used in the final calculation
 	for {		//Mileage input loop start
 		fmt.Println("How much is the current mileage?")
-		fmt.Scan(&mileageString)
-		cleanMileageString := strings.TrimPrefix(mileageString, "$")		//In case the user adds the $ to their input, trims $
-		m, err := strconv.ParseFloat(cleanMileageString, 64)
+		fmt.Scan(&inputString)
+		cleanInputString := strings.TrimPrefix(inputString, "$")		//In case the user adds the $ to their input, trims $
+		m, err := strconv.ParseFloat(cleanInputString, 64)
 		mileage = m		//Assigning the ParseFloat directly to mileage shadows the variable outside the loop
 		if err == nil {
 			break
@@ -33,27 +32,43 @@ func main () {
 	fmt.Println("Enter \"help\" for command list, enter \"exit\" to end shift.")
 	
 	for {
-		fmt.Println("Enter the tip for delivery #", len(orders)+1)
-		fmt.Scan(&tipString)
-		cleanTipString := strings.TrimPrefix(tipString, "$")		//Same with mileage input, trims off potential $'s
-		switch {
-			case tipString == "exit":
+		fmt.Println("Enter the tip for delivery #", strconv.Itoa(len(orders)+1))
+		fmt.Scan(&inputString)
+		cleanInputString := strings.TrimPrefix(inputString, "$")		//Same with mileage input, trims off potential $'s
+		switch {		//Interpret different input commands
+			case inputString == "exit":		//End shift and print totals
 				clockOut(orders, mileage)
 				break
-			case tipString == "help":
+			case inputString == "help":		//Print out help menu
 				fmt.Println(helpString)
 				break
-			default:
-				tip, err := strconv.ParseFloat(cleanTipString, 64)
+			case inputString == "list":		//Print out list of orders
+				listOrders(orders)
+				break
+			case inputString == "cancel":		//Cancel an order using order's ID
+				fmt.Println("Here's a list of all deliveries made. Please input the order number of the delivery you want cancelled.")
+				listOrders(orders)
+				fmt.Scan(&inputString)
+				id, err := strconv.Atoi(inputString)
 				if err == nil {
-					newOrder := Order{tip}
+					cancelOrder(orders, id-1)
+					fmt.Println(fmt.Sprintf("Order has been cancelled\nNew Total Tips: $%.2f", getTotalTips(orders)))
+				} else {
+					fmt.Println("Invalid Input")
+				}
+				break
+			default:		//Append new order to orders slice setting tip and id
+				tip, err := strconv.ParseFloat(cleanInputString, 64)
+				if err == nil {
+					id := len(orders)+1
+					newOrder := Order{tip, id}
 					orders = append(orders, newOrder)
 					fmt.Println(fmt.Sprintf("Total Tips: $%.2f", getTotalTips(orders)))		//Handy display of current total
 				} else {
 					fmt.Println("Invalid Input")
 				}
 		}		//End of switch case
-		if tipString == "exit" {
+		if inputString == "exit" {
 			break
 		}
 	}		//End of tip input For Loop
@@ -62,14 +77,15 @@ func main () {
 
 type Order struct {		//Order struct
 	tip float64
+	id int
 }
 
-func (o Order) getTip() float64 {
+func (o Order) getTip() float64 {		//return tip
 	return o.tip
 }
 
-func (o Order) cancelOrder() {		//To be implemented
-	o.tip = 0.0
+func cancelOrder(o []Order, id int) {		//Takes in the orders slice and id, sets tip to 0
+	o[id].tip = 0.0
 	return
 }
 
@@ -86,4 +102,15 @@ func getTotalTips (o []Order) float64{		//Returns total of tips
 	}
 	return totalTips
 
+}
+
+func listOrders(o []Order) {		//List all orders
+	if len(o) == 0 {
+		fmt.Println("You haven't taken any deliveries yet!")
+	} else {
+		for i := 0; i < len(o); i++ {
+			fmt.Println(fmt.Sprintf("Order #%d: $%.2f", o[i].id, o[i].tip))
+		}
+	}
+	return
 }
